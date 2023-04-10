633921a3d9b67f86e903b42d36f02b4d58152e0c
irondijonathan <irondicjonathan@gmail.com>
2023-04-10 13:56:12 -0700

    test

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 #include "main.h"
 
 
@@ -13,10 +12,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	if (index > 63)
 		return (-1);
 
-	*n ^= (1 << index);
-
+	*n = ((1UL << index) | *n);
 	return (1);
 }
+
