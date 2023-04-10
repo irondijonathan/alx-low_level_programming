46de59ed2a978b57602d5877912f1c8215376818
irondijonathan <irondicjonathan@gmail.com>
2023-04-10 12:52:55 -0700

    test

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars.
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ * Otherwise - the converted number.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, mult = 1;
+	int len = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[len])
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		len++;
+	}
+
+	for (len -= 1; len >= 0; len--)
+	{
+		num += (b[len] - '0') * mult;
+		mult *= 2;
+	}
+
+	return (num);
+}
+
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,3 @@
+0x14-bit_manipulation
+Irondi Jonathan 
+This is my 0x14-bit_manipulation task
